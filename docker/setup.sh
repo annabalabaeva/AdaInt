35931cd33d159e1f5774514e93a4ec7e825e5fa7
Anna <Balabaeva>
2022-12-07 20:16:15 +0400

    update setup script

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,6 +1,5 @@
-conda create -n adaint python=3.7.10
-conda activate adaint
-conda install -c pytorch pytorch=1.8.1 torchvision cudatoolkit=10.2 -y
+pip install pytorch==1.8.1 torchvision cudatoolkit==10.2 -y
+python --version
 pip install -r requirements.txt
 pip install -v -e .
 python adaint/ailut_transform/setup.py install
