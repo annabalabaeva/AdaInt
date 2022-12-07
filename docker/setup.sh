9d6193b40a12feea1cdc75afde6f6d667484fbbc
Anna <Balabaeva>
2022-12-07 23:19:51 +0400

    update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,6 +1,6 @@
-pip3 install pytorch==1.8.1 torchvision cudatoolkit==10.2 -y
-python3 --version
-pip3 install -r requirements.txt
-pip3 install -v -e .
-python3 adaint/ailut_transform/setup.py install
+python3.7 -m pip install pytorch==1.8.1 torchvision cudatoolkit==10.2 -y
+python3.7 --version
+python3.7 -m pip install -r requirements.txt
+python3.7 -m pip install -v -e .
+python3.7 adaint/ailut_transform/setup.py install
 
