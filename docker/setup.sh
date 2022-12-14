9c7a2067a2ecfc36ae09692c254d4deda90613e0
Anna <Balabaeva>
2022-12-14 17:56:29 +0400

    try to change ailut setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,5 +2,5 @@ export FORCE_CUDA="1"
 python3.7 --version
 python3.7 -m pip install -r requirements.txt
 python3.7 -m pip install -v -e .
-#python3.7 adaint/ailut_transform/setup.py install
-python3.7 -m pip install adaint/ailut_transform/ailut-1.5.0-cp37-cp37m-linux_x86_64.whl
+python3.7 adaint/ailut_transform/setup.py install
+#python3.7 -m pip install adaint/ailut_transform/ailut-1.5.0-cp37-cp37m-linux_x86_64.whl
